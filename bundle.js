89d646c2d214cf24396a3fa95c794fe0ad241be7
xuhaiqiang <xuhaiqiang@xiaoheiban.cn>
2021-08-30 17:39:00 +0800

    feat: 完成

--- bundle.js
+++ bundle.js
@@ -1,7 +1,72 @@
-// 获取主入口文件
 const fs = require('fs');
+const path = require('path');
+const parser = require('@babel/parser');
+const traverse = require('@babel/traverse').default;
+const babel = require('@babel/core');
 const getModuleInfo = (file) => {
   const body = fs.readFileSync(file, 'utf-8');
-  console.log(body);
+  const ast = parser.parse(body, {
+    sourceType: 'module' //表示我们要解析的是ES模块
+  });
+  const deps = {};
+  traverse(ast, {
+    ImportDeclaration({ node }) {
+      const dirname = path.dirname(file);
+      const abspath = './' + path.join(dirname, node.source.value);
+      deps[node.source.value] = abspath;
+    }
+  });
+
+  const { code } = babel.transformFromAst(ast, null, {
+    presets: ['@babel/preset-env']
+  });
+  const moduleInfo = { file, deps, code };
+  return moduleInfo;
 };
-getModuleInfo('./src/index.js');
+
+const parseModules = (file) => {
+  const entry = getModuleInfo(file);
+  const temp = [entry];
+  const depsGraph = {};
+  for (let i = 0; i < temp.length; i++) {
+    const deps = temp[i].deps;
+    if (deps) {
+      for (const key in deps) {
+        if (deps.hasOwnProperty(key)) {
+          temp.push(getModuleInfo(deps[key]));
+        }
+      }
+    }
+  }
+  temp.forEach((moduleInfo) => {
+    depsGraph[moduleInfo.file] = {
+      deps: moduleInfo.deps,
+      code: moduleInfo.code
+    };
+  });
+  return depsGraph;
+};
+
+const bundle = (file) => {
+  const depsGraph = JSON.stringify(parseModules(file));
+  return `(function (graph) {
+      function require(file) {
+          function absRequire(relPath) {
+              return require(graph[file].deps[relPath])
+          }
+          var exports = {}; // ;很关键
+          (function (require,exports,code) {
+              eval(code)
+          })(absRequire,exports,graph[file].code)
+          return exports
+      }
+      require('${file}')
+  })(${depsGraph})`;
+};
+
+const content = bundle('./src/index.js');
+
+// console.log(content);
+
+fs.mkdirSync('./dist');
+fs.writeFileSync('./dist/bundle.js', content);
